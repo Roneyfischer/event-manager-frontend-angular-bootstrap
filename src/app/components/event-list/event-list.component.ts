1024014e8f2d3d92a2e25e572c6e87664d60d648
Rôney Fischer <fischer.roney@gmail.com>
2022-11-20 15:43:05 -0300

    same

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-list.component.css'],
 })
 export class EventListComponent {
-
-
+  temporaryDescription = `Hic dolorem nemo et odit suscipit rem laboriosam deleniti qui voluptas asperiores et autem beatae qui distinctio necessitatibus id numquam dignissimos. Et galisum numquam rem consectetur minima non exercitationem optio. Sit consequatur nobis et minima vitae qui tempore perferendis est necessitatibus dolores. Eum mollitia facere et dolore culpa et aliquid labore. A itaque omnis et sunt velit et illo. Est voluptatum!`;
+  url = "https://localhost:4200"
   events = [
     {
       name: `Evento 01`,
-      liteDescription: `Ipsum lorem`,
+      liteDescription: this.temporaryDescription,
       category: `Educação`,
       group: `pastores`,
       date: '25/11/2022',
@@ -21,7 +21,7 @@ export class EventListComponent {
     },
     {
       name: `Evento 01`,
-      liteDescription: `Ipsum lorem`,
+      liteDescription: this.temporaryDescription,
       category: `Educação`,
       group: `pastores`,
       date: '25/11/2022',
@@ -31,7 +31,7 @@ export class EventListComponent {
     },
     {
       name: `Evento 01`,
-      liteDescription: `Ipsum lorem`,
+      liteDescription: this.temporaryDescription,
       category: `Educação`,
       group: `pastores`,
       date: '25/11/2022',
